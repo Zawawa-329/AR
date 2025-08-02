f0d733dd1b91544af06740171d91db3df9ff93a5
SayaKoike <ksayak127@gmail.com>
2025-08-02 14:36:32 +0900

    shima

--- AR/ContentView.swift
+++ AR/ContentView.swift
@@ -36,17 +36,27 @@ struct ContentView: View {
     var body: some View {
         ZStack(alignment: .topLeading) {
             RealityView { content in
-                let model = Entity()
+               /* let model = Entity()
                 let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
                 let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
                 model.components.set(ModelComponent(mesh: mesh, materials: [material]))
-                model.position = [0, 0.05, 0]
+                model.position = [0, 0.05, 0]*/
                 
                 let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-                anchor.addChild(model)
+               
                 content.add(anchor)
                 
                 content.camera = .spatialTracking
+                
+                Task{
+                    do{
+                        let entitiy=try await Entity(named:"shima")
+                        anchor.addChild(entitiy)
+                    }catch{
+                        print("error")
+                    }
+                }
+                
             }
             .edgesIgnoringSafeArea(.all)
             
